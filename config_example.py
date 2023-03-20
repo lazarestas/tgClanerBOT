110a48eeb075bddebfa2c20896b0a85a60e3c4ce
lazarestas <80478760+lazarestas@users.noreply.github.com>
2023-03-20 18:39:54 +0300

    created config example
    This file should be added to gitignore for privacy of your bot

---
+++ config_example.py
@@ -0,0 +1,5 @@
+# Set your Telegram bot token and chat ID here
+# This file should be added to gitignore for privacy of your bot
+# original name of file is config.py
+BOT_TOKEN = "sddfnmg;sdhf;smbn-23945pro;j3q459#@$"
+CHAT_ID = "122312312312"
